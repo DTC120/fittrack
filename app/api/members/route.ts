c15f2d4fcc372217503fbe45babfac78b8972201
DTC120 <diego.trejo0903@gmail.com>
2026-05-01 10:19:59 -0600

    Agregar dynamic exports a API routes para prevenir prerendering

--- app/api/members/route.ts
+++ app/api/members/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const members = await prisma.member.findMany({
     include: {
